b1630e6b30e6c8b2e9c9d997e9dc21e91f9d1deb
Shun <tanakagoro0319@yahoo.ne.jp>
2023-01-13 17:11:27 +0900

    STEP4完了

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,26 +1,37 @@
+import {NavigationMixin} from 'lightning/navigation';
 import { LightningElement, wire } from 'lwc';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
 /** BearController.getAllBears() Apex method */
-// import getAllBears from '@salesforce/apex/BearController.getAllBears';
 import searchBears from '@salesforce/apex/BearController.searchBears';
 
-export default class BearList extends LightningElement {
+export default class BearList extends NavigationMixin(LightningElement) {
   searchTerm = '';
   @wire(searchBears, { searchTerm: '$searchTerm' })
   bears;
-  appResources = {
-    bearSilhouette: `${ursusResources}/standing-bear-silhouette.png`,
-  };
 
-  handleSearchTermChange(event){
+  handleSearchTermChange(event) {
     window.clearTimeout(this.delayTimeout);
     const searchTerm = event.target.value;
-    this.delayTimeout = setTimeout(() =>{
+    this.delayTimeout = setTimeout(() => {
       this.searchTerm = searchTerm;
-    },300)
+    }, 300);
   }
 
   get hasResults() {
     return (this.bears.data.length > 0);
+  }
+
+  handleBearView(event){
+    // Get bear record id from bearview event
+		const bearId = event.detail;
+		// Navigate to bear record page
+    this[NavigationMixin.Navigate]({
+      type: 'standard__recordPage',
+      attributes: {
+        recordId: bearId,
+        objectApiName: 'Bear__c',
+        actionName: 'view'
+      }
+    });
   }
 }
